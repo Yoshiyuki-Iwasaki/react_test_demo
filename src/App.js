274b9e156183eb48267113af9e5d053143276fe1
Yoshiyuki Iwasaki <armada3524@gmail.com>
2021-02-15 14:31:19 +0900

    [fix] render test実装

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
-import React from 'react';
-import logo from './logo.svg';
-import { Counter } from './features/counter/Counter';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import { Counter } from "./features/counter/Counter";
+import "./App.css";
 
 function App() {
+  const output = text => {
+    console.log(text);
+  };
+
   return (
     <div className="App">
       <header className="App-header">
